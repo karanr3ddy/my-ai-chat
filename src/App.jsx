2fe9fbba1ee8da7aead75f9e8769f9d3e19c3c9b
Karan Reddy <karanreddy102008@gmail.com>
2024-01-23 11:23:21 +0530

    UI change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import { ChatOpenAI } from "@langchain/openai";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { Spinner } from 'flowbite-react';
+import _ from 'lodash';
 
 import './App.css'
 
@@ -12,49 +14,82 @@ function App() {
   });
 
   const [messages, setMessages] = useState();
-  const [query, setQuery] = useState();
+  const [chatHistory, setChatHistory] = useState([]);
+  const [query, setQuery] = useState('');
+  const [isFetching, setIsFetching] = useState(false);
 
 
 
-  const getResp = async (promptAsString) => {
-    const response = await chatModel.invoke(promptAsString);
-    console.log(response);
-    setMessages(response);
+  const getResp = async (userInput) => {
+
+    setMessages(null);
+    setIsFetching(true);
+
+    try {
+
+      const response = await chatModel.stream(userInput);
+      console.log(response);
+      setChatHistory([...chatHistory, response])
+      setMessages(response);
+    } catch (error) {
+      console.error('getResp', error)
+    } finally {
+      setIsFetching(false);
+    }
+
   }
 
   useEffect(() => {
+    console.log(chatHistory)
+  }, [chatHistory]);
 
-
-
+  const handleClick = async () => {
+    setQuery('')
     const prompt = ChatPromptTemplate.fromMessages([
-      ["system", "You are a world class technical documentation writer."],
       ["user", "{input}"],
     ]);
-    const promptAsString = "Human: Tell me a short joke about ice cream";
-
-
-
-    // getResp(promptAsString);
-
-
-  }, []);
-
-  const handleClick = () => {
-    getResp(query);
+    const promptValue = await prompt.invoke({ input: query });
+    const promptAsString = promptValue.toString();
+    console.log(promptValue)
+    chatHistory.push(...promptValue.messages);
+    setChatHistory([...chatHistory,])
+    getResp(promptAsString);
   }
 
+  
+
+  function renderChats() {
+    if(_.isEmpty(chatHistory)) return null;
+
+    return  chatHistory
+    .filter((chat) => !_.isEmpty(chat.content))
+    .map((chat, index) => 
+    <p key={index} className='p-3 border rounded mt-5'>
+      {chat?.content}</p>);
+  }
+
+
   return (
-    <>
-      <h1>Hello</h1>
+    <div className='h-screen w-screen p-5 flex flex-col justify-end'>
+  
+      <div>
+
+        {renderChats()}
+        {isFetching ? <div className="text-left p-5">
+          <Spinner aria-label="Center-aligned spinner example" size="md" />
+        </div> : null
+        }
+      </div>
       <div className='flex flex-col gap-4 mt-5'>
         <label className='text-2xl'>Type query</label>
-        <input type='text' className='rounded text-xl p-3' value={query} onChange={(e) => setQuery(e.target.value)} />
+        <input type='text' className='rounded text-xl p-3 bg-gray-900' value={query} onChange={(e) => setQuery(e.target.value)} />
         <button onClick={handleClick}>Send</button>
       </div>
 
-      <p className='p-8 text-xl mt-5'>{messages?.content}</p>
-    </>
+
+    </div>
   )
+
 }
 
 export default App
